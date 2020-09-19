2cbff3fd3ca2f0a676fc930b7a794eb5a6dcfc39
nguyenanhthoai99 <69306469+nguyenanhthoai99@users.noreply.github.com>
2020-09-19 19:27:44 +0700

    gửi email

---
+++ fontend/layouts/styles.php
@@ -0,0 +1,15 @@
+<!-- Liên kết bootstrap -->
+<link href="/web02/asset/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+<script src="/web02/assets/vendor/jquery/jquery-3.5.1.min.js"></script>
+
+<!-- Thư viện Jquery validation -->
+<script src="/web02/assets/vendor/jquery-validation/jquery.validate.min.js"></script>
+<script src="/web02/assets/vendor/jquery-validation/localization/messages_vi.min.js"></script>
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="/web02/asset/vendor/bootstrap/css/bootstrap.min.css" type="text/css" />
+<!-- Font awesome -->
+<link rel="stylesheet" href="/web02/asset/vendor/font-awesome/css/font-awesome.min.css" type="text/css" />
+
+<!-- Custom css - Các file css do chúng ta tự viết -->
+<link rel="stylesheet" href="/web02/asset/frontend/css/style.css" type="text/css" />
---
+++ fontend/pages/home.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Trang Chủ</title>
+    <?php include_once(__DIR__.'/../layouts/styles.php')?>
+</head>
+<body>
+    <?php include_once(__DIR__.'/../layouts/partials/header.php');?>
+    <div class="container">
+        <div class="row">
+           <?php include_once(__DIR__.'/contact.php')?>
+        </div>
+    </div>
+    <?php include_once(__DIR__.'/../layouts/partials/footer.php');?>
+</body>
+</html>
